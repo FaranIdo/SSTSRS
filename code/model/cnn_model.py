a102e9172e1bc7dd976daa7b9ee9c6287a32b350
ido <FaranIdoF@gmail.com>
2024-08-31 00:05:27 +0300

    make all eval models work

--- code/model/cnn_model.py
+++ code/model/cnn_model.py
@@ -16,15 +16,29 @@ class CNNNDVIModel(nn.Module):
             layers.append(nn.Conv1d(in_channels=num_channels, out_channels=num_channels, kernel_size=kernel_size, padding=kernel_size // 2))
             layers.append(nn.ReLU())
 
-        layers.append(nn.Conv1d(in_channels=num_channels, out_channels=1, kernel_size=kernel_size, padding=kernel_size // 2))
-
         self.cnn_layers = nn.Sequential(*layers)
+
+        # Add a final layer to reduce to a single channel
+        self.final_conv = nn.Conv1d(in_channels=num_channels, out_channels=1, kernel_size=1)
+
+        # Add a linear layer for the final prediction
+        self.linear = nn.Linear(sequence_length, 1)
 
     def forward(self, ndvi: torch.Tensor, years: torch.Tensor, seasons: torch.Tensor) -> torch.Tensor:
         # Add a channel dimension to the input
         ndvi = ndvi.unsqueeze(1)
 
         # Pass through CNN layers
-        output = self.cnn_layers(ndvi).squeeze(1)
+        output = self.cnn_layers(ndvi)
 
-        return output
+        # Reduce to a single channel
+        output = self.final_conv(output)
+
+        # Remove the channel dimension
+        output = output.squeeze(1)
+
+        # Predict the next value
+        prediction = self.linear(output)
+
+        # Remove the extra dimension to have a 1D output of NDVI values
+        return prediction.squeeze(1)
